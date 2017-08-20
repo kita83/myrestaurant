47814c6b65e83c8a8619bb876668b5a83c1b0c3b
strkita <kita83@gmail.com>
2017-08-20 13:25:06 +0900

    django-bootstrap導入

--- myrecommendations/settings.py
+++ myrecommendations/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap3',
     'myrestaurants',
 ]
 
@@ -63,6 +64,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+            ],
+            'builtins': [
+                'bootstrap3.templatetags.bootstrap3',
             ],
         },
     },
--- myrestaurants/views.py
+++ myrestaurants/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.core.urlresolvers import reverse, reverse_lazy
